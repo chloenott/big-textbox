9768168ef33a2e39af3b88fc1cd4ea9f692f30c1
CHLOE NOTT <CHLOE@NOTT.EMAIL>
2021-09-05 15:23:20 -0700

    Initial Commit

---
+++ big-text/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  big-text
+//
+//  Created by Chloe Nott on 9/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
