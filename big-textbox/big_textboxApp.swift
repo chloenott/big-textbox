86f3fb6e5b7ff81716cdec2d33cd271cb6e9f3fb
Chloe Nott <chloe@nott.email>
2021-09-11 07:25:26 -0700

    Initial Commit

---
+++ big-textbox/big_textboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  big_textboxApp.swift
+//  big-textbox
+//
+//  Created by Chloe Nott on 9/11/21.
+//
+
+import SwiftUI
+
+@main
+struct big_textboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
